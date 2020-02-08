19de4ab3fa35c5a3b7cd3267a1bb36904b9a963a
chen0207-lh <57855538+chen0207-lh@users.noreply.github.com>
2020-02-08 15:52:41 +0800

    首页完成
    首页部分完成，并封装了request请求库

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,102 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+import {
+  request
+} from "../../request/request.js"
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    swiper_list: [],
+    menu_list: [],
+    floor_list:[]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.getSwiperList();
+    this.getmenuList();
+    this.getfloorList()
+  },
+  //获取路播图数据
+  getSwiperList: async function(e) {
+    const swiper_list = await request({
+      url: '/home/swiperdata'
     })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      swiper_list
     })
+    // console.log(swiper_list)
+  },
+  // 获取菜单导航数据
+  getmenuList: async function(e) {
+    const menu_list = await request({
+      url: '/home/catitems'
+    })
+    this.setData({
+      menu_list
+    })
+    // console.log(menu_list)
+  },
+  // 获取楼层数据
+  getfloorList: async function (e){
+    const floor_list = await request({
+      url:'/home/floordata'
+    })
+    this.setData({
+      floor_list
+    })
+    console.log(floor_list)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
   }
 })
